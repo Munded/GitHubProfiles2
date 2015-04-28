a2d29e1c600c1a0d4d578c5cf237f4172711cad7
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-04-28 14:17:49 +0100

    github api sorted

--- js/controller/gitUserSearchController.js
+++ js/controller/gitUserSearchController.js
@@ -2,7 +2,7 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource',function($resource) {
 
   
-  var searchResource = $resource('https://api.github.com/search/users');
+  var searchResource = $resource('https://api.github.com/search/users?access_token=25a7ecd4b4211b62b4cb5d5e5d453305838fbaa3');
   var self = this;
   self.doSearch = function(){
     // console.log(self.searchTerm)
