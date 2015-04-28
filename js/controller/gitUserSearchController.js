41b7cb2186e371b2079cb541981edcf3861542ce
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-04-28 15:30:41 +0100

    stored previous searches

--- js/controller/gitUserSearchController.js
+++ js/controller/gitUserSearchController.js
@@ -4,10 +4,13 @@ githubUserSearch.controller('GitUserSearchController', ['$resource',function($re
   
   var searchResource = $resource('https://api.github.com/search/users?access_token=25a7ecd4b4211b62b4cb5d5e5d453305838fbaa3');
   var self = this;
+  this.pastSearch = []
   self.doSearch = function(){
     // console.log(self.searchTerm)
     self.searchResult = searchResource.get( 
       { q : self.searchTerm } 
       );
+    this.pastSearch.push(" " + self.searchTerm)
+    this.printResults = this.pastSearch.toString();
   };
 }]);
